fcecfc1ba112408266fa2090fe207bc5eb08f7bd
Renato Britto <renatobritto@protonmail.com>
2023-12-16 18:42:09 -0300

    sáb 16 dez 2023 18:42:09 -03

--- README.md
+++ README.md
@@ -30,3 +30,48 @@ Prezados alunos, o Argumento do perigeu, RAAN e a Anomalia Verdadeira devem fica
 A maior aproximação do satélite a antena aconteceu em 01 Jan 2024 18:38:48.602. Nesse aproximação, a simulação diz que o satélite ficou a 1548 km da faculdade do gama.
 
 ![](Screenshot%20from%202023-12-16%2017-12-22.png)
+
+Devido a inconsistencia..
+https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc
+
+cp ../GMAT/R2022a/output/ContactLocator1.txt ContactLocator4.txt
+
+Erros nas respostas:
+
+- Calculei com orbita de 400 KM acima da terra (e=0)
+- Nenhum resultado deveria ser menor que 400, alguns perto de 400. 
+- Nenhum resultado deveria ser maior que 2200. 
+
+Informações sobre lista de contatos com orbita 400km e=0:
+Mean: 1157.1662804674472
+Standard Deviation: 393.9903079456652
+Max Range: 1665.548
+Min Range: 403.719
+Events in target of <600: 190
+
+Ou seja, temos um resultado aqui perfeitamente dentro do intervalo de respostas proposto.
+
+Agora
+
+Vou conferir parametros da orbita
+
+
+
+## orbita
+
+PERI = 500km
+APO = 20000km
+INC = 20
+RAAN = 360
+AOP = 356.352
+TA = 90
+
+Somando o raio da terra (6378.1366 km), temos
+
+PERI = 6878.1366 km
+APO = 26378.136599999998 km
+INC = 20
+RAAN = 360
+AOP = 356.352
+TA = 90
+
--- filter.py
+++ filter.py
@@ -1,3 +1,4 @@
+import math
 import csv
 
 import sys
@@ -14,15 +15,35 @@ with open(dataset_filename, "r") as f:
     dataset = list(reader)
 
 # earth radius
-R = 6371
+R = 6378.1366
 # max range
 range = 600
 # range with earth radius
 range_earth = range + R
 
-
 # get all ranges
-ranges = [float(data["Range (km)"]) for data in dataset]
+# ranges = [float(data["Range (km)"]) for data in dataset]
+
+ranges = []
+for data in dataset:
+    d = float(data["Range (km)"])
+    o = float(data["Elevation (deg)"])
+    o = math.radians(o)
+
+    y = R
+
+    vx = y + math.sin(o) * d
+    vy = math.cos(o) * d
+
+    alt = math.sqrt(vx**2 + vy**2) - R
+
+    res = alt
+
+    # res = r * math.cos(e)
+    ranges.append(res)
+
+    print("D:", d, "O (deg):", math.degrees(o), "alt:\t", res)
+    # if res < range:
 
 # calculate standard deviation
 mean = sum(ranges) / len(ranges)
@@ -30,8 +51,8 @@ variance = sum([(r - mean) ** 2 for r in ranges]) / len(ranges)
 std_dev = variance**0.5
 
 # save all ranges to file
-with open("ranges.txt", "w") as f:
-    f.write("\n".join([str(r) for r in ranges]))
+# with open("ranges.txt", "w") as f:
+#     f.write("\n".join([str(r) for r in ranges]))
 
 
 # print results
@@ -42,10 +63,11 @@ print("Min Range:", min(ranges))
 
 # filter dataset
 filtered = []
-for data in dataset:
-    if float(data["Range (km)"]) < range:
-        filtered.append(data)
+for val in ranges:
+    if val < range:
+        filtered.append(val)
 
-print("FILTERED:", filtered)
+
+# print("FILTERED:", filtered)
 
 print(f"Events in target of <{range}:", len(filtered))
